1d4f79957794f122c294c5c19613606bc5616f84
Justin Stewart <j_stewart8@mail.fhsu.edu>
2024-09-16 02:49:47 -0500

    set up yFinace to pull tickers, day High, and extract the last 10 rows of a dataframe.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 ### INF601 - Advanced Programming in Python
 ### Justin Stewart
 ### Mini Project 1
+
+import pprint
+import yfinance as yf
+msft = yf.Ticker("MSFT")
+
+mytickers = ["AMD", "INTC", "NVDA", "AMC", "GME"]
+mydata = {}
+
+for ticker in mytickers:
+    result = yf.Ticker(ticker)
+    mydata[ticker] = {'Ticker': ticker,
+                      'dailyHigh': result.info['dayHigh']}
+
+pprint.pprint(mydata)
+
+hist = msft.history(period="1mo")
+
+ten_days = hist.iloc[-10:]
+
+pprint.pprint(ten_days)
